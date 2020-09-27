d4a176758d350d3bd1f6a4ebf2fd76c1c4deb17d
Hamdy <hamdy.t@trufla.com>
2020-09-27 10:27:47 +0200

    Added scoring uri and key to the endpoint script

--- endpoint.py
+++ endpoint.py
@@ -3,9 +3,9 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = 'http://c69d6278-d05a-43f2-be9f-ffb30a46d453.eastus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = ''
+key = 'kpJCONmLL6X978sMqeiw0YvxftDk8rVR'
 
 # Two sets of data to score, so we get two results back
 data = {"data":
